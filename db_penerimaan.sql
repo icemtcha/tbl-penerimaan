9531ed6a94f55947925b7fce3b1125cc29302ef1
icemtcha <mtchalattebl@gmail.com>
2026-03-26 12:46:28 +0700

    Update db_penerimaan.sql

--- db_penerimaan.sql
+++ db_penerimaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2026 at 06:07 AM
+-- Generation Time: Mar 26, 2026 at 06:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,49 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_penerimaan`
+-- Table structure for table `tbl_jenis_penerimaan`
 --
 
-CREATE TABLE `tb_penerimaan` (
+CREATE TABLE `tbl_jenis_penerimaan` (
   `id` int(11) NOT NULL,
-  `kode_penerimaan` varchar(50) DEFAULT NULL,
-  `sub_akun` varchar(50) DEFAULT NULL,
+  `kd_penerimaan` varchar(50) DEFAULT NULL,
+  `subakun_pendapatan` varchar(50) DEFAULT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `akun_harta` varchar(50) DEFAULT NULL,
   `akun_pendapatan` varchar(50) DEFAULT NULL,
-  `akun_apbs` varchar(50) DEFAULT NULL,
-  `terikat` varchar(100) DEFAULT NULL,
+  `akun_pendapatan_apbs` varchar(50) DEFAULT NULL,
+  `akun_pendapatan_terikat` varchar(50) DEFAULT NULL,
   `keterangan` text DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL
+  `status` varchar(20) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_modified` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_penerimaan`
+-- Dumping data for table `tbl_jenis_penerimaan`
 --
 
-INSERT INTO `tb_penerimaan` (`id`, `kode_penerimaan`, `sub_akun`, `nama`, `akun_harta`, `akun_pendapatan`, `akun_apbs`, `terikat`, `keterangan`, `status`) VALUES
-(1, '22', '222', '222', '22', '222', '222', '222', 'aktif', 'aktif');
+INSERT INTO `tbl_jenis_penerimaan` (`id`, `kd_penerimaan`, `subakun_pendapatan`, `nama`, `akun_harta`, `akun_pendapatan`, `akun_pendapatan_apbs`, `akun_pendapatan_terikat`, `keterangan`, `status`, `date_created`, `date_modified`) VALUES
+(1, '22', '222', '222', '22', '222', '222', '222', 'aktif', 'aktif', '2026-03-26 12:44:14', '2026-03-26 12:44:14');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_penerimaan`
+-- Indexes for table `tbl_jenis_penerimaan`
 --
-ALTER TABLE `tb_penerimaan`
+ALTER TABLE `tbl_jenis_penerimaan`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `kode_penerimaan` (`kode_penerimaan`);
+  ADD UNIQUE KEY `kd_penerimaan` (`kd_penerimaan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tb_penerimaan`
+-- AUTO_INCREMENT for table `tbl_jenis_penerimaan`
 --
-ALTER TABLE `tb_penerimaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `tbl_jenis_penerimaan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
